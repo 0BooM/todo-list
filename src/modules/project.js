a808676b5e3ef188e791f3d4606013cf00d9e04a
0BooM <149960425+0BooM@users.noreply.github.com>
2024-07-31 21:12:18 +0200

    Implemented adding system to particular project
    It will be implemented soon. Right now it creates new item and adds it to the list of items in our Projects class

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,8 +1,14 @@
+import todoItem from "./todoItem.js";
+
 export default class Project {
   static projectId = 1;
   static items = [];
   constructor(title) {
     this.title = title;
     this.projectId = Project.projectId++;
+  }
+  addItem(title, description, dueDate) {
+    let newTodoItem = new todoItem(title, description, dueDate);
+    items.push(newTodoItem);
   }
 }
