dde16b77036dc7b637d94e1204c80fcb74f6db80
0BooM <149960425+0BooM@users.noreply.github.com>
2024-08-02 21:07:32 +0200

    Update: todoItemManager now renders an item with working button that shows and hides desc

--- src/modules/todoItemManager.js
+++ src/modules/todoItemManager.js
@@ -4,22 +4,48 @@ const todoItemManager = (() => {
   function renderItem(item) {
     let todoElement = document.createElement("li");
     todoElement.classList.add("to-do-element");
-    todoElement.innerHTML = `<div class="sections">
-            <div class="left-section">
-              <input type="checkbox" class="to-do-checbox" />
-              <p>${item.title}</p>
-            </div>
-            <div class="right-section">
-              <p class="due-date">Due: 04-08-2024</p>
+    let sectionDiv = document.createElement("div");
+    sectionDiv.classList.add("sections");
+
+    let leftSection = document.createElement("div");
+    leftSection.classList.add("left-section");
+    leftSection.innerHTML = `<input type="checkbox" class="to-do-checbox" />
+              <p>${item.title}</p>`;
+
+    let rightSection = document.createElement("div");
+    rightSection.classList.add("right-section");
+    rightSection.innerHTML = `<p class="due-date">Due: 04-08-2024</p>
               <span class="remove-icon material-symbols-outlined">
                 delete
-              </span>
-            </div>
-          </div>
-          <div class="description">
-            ${item.title}
-          </div>`;
+              </span>`;
+
+    let expandBtn = document.createElement("span");
+    expandBtn.classList.add("expand-icon", "material-symbols-outlined");
+    expandBtn.innerHTML = `expand_circle_down`;
+    rightSection.append(expandBtn);
+
+    let desc = document.createElement("div");
+    desc.classList.add("description");
+    desc.innerHTML = `${item.description}`;
+
+    todoElement.appendChild(sectionDiv);
+    sectionDiv.appendChild(leftSection);
+    sectionDiv.appendChild(rightSection);
+
+    expandDescOfItem(todoElement, desc, expandBtn);
     return todoElement;
+  }
+
+  function expandDescOfItem(todoElement, desc, expandBtn) {
+    expandBtn.addEventListener("click", () => {
+      if (desc.parentNode) {
+        todoElement.removeChild(desc);
+        expandBtn.innerHTML = `expand_circle_down`;
+      } else {
+        todoElement.appendChild(desc);
+        expandBtn.innerHTML = `expand_circle_up`;
+      }
+    });
   }
   return {
     renderItem,
