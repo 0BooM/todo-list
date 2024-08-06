847706afb6d3a4b188f206793c623c6c8795ad6b
0BooM <149960425+0BooM@users.noreply.github.com>
2024-08-06 16:45:02 +0200

    Update: addItem() accepts dueDate

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,14 +1,20 @@
-import todoItem from "./todoItem.js";
+import TodoItem from "./todoItem.js";
 
 export default class Project {
   static projectId = 1;
-  static items = [];
+  items = [
+    new TodoItem(
+      "Initial Task",
+      "This is an initial task description",
+      "2024-08-04"
+    ),
+  ];
   constructor(title) {
     this.title = title;
     this.projectId = Project.projectId++;
   }
   addItem(title, description, dueDate) {
-    let newTodoItem = new todoItem(title, description, dueDate);
-    items.push(newTodoItem);
+    let newTodoItem = new TodoItem(title, description, dueDate);
+    this.items.push(newTodoItem);
   }
 }
