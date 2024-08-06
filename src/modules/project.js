c5864ca7229976ee70be01db1e811b3c9d044134
0BooM <149960425+0BooM@users.noreply.github.com>
2024-08-06 17:18:34 +0200

    Update: add status variable

--- src/modules/project.js
+++ src/modules/project.js
@@ -7,15 +7,23 @@ export default class Project {
       "Initial Task",
       "This is an initial task description",
       "2024-08-04",
-      "high"
+      "high",
+      "done"
     ),
   ];
   constructor(title) {
     this.title = title;
     this.projectId = Project.projectId++;
   }
-  addItem(title, description, dueDate, priority) {
-    let newTodoItem = new TodoItem(title, description, dueDate, priority);
+  addItem(title, description, dueDate, priority, checked = false) {
+    let newTodoItem = new TodoItem(
+      title,
+      description,
+      dueDate,
+      priority,
+      checked
+    );
     this.items.push(newTodoItem);
+    console.log(this.items);
   }
 }
--- src/modules/todoItem.js
+++ src/modules/todoItem.js
@@ -2,11 +2,12 @@ import { format, parseISO } from "date-fns";
 
 export default class TodoItem {
   static itemId = 1;
-  constructor(title, description, dueDate, priority) {
+  constructor(title, description, dueDate, priority, checked = false) {
     this.title = title;
     this.description = description;
     this.dueDate = format(parseISO(dueDate), "dd/MM/yyyy");
     this.priority = priority;
+    this.status = checked ? "done" : "not-done";
     this.itemId = TodoItem.itemId++;
   }
 }
