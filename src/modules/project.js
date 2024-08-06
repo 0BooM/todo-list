b8efc5267a6f30eeb5127aa6b6570c5e2f02e46f
0BooM <149960425+0BooM@users.noreply.github.com>
2024-08-06 17:05:36 +0200

    Update: Items now accept priority value

--- src/modules/project.js
+++ src/modules/project.js
@@ -6,15 +6,16 @@ export default class Project {
     new TodoItem(
       "Initial Task",
       "This is an initial task description",
-      "2024-08-04"
+      "2024-08-04",
+      "high"
     ),
   ];
   constructor(title) {
     this.title = title;
     this.projectId = Project.projectId++;
   }
-  addItem(title, description, dueDate) {
-    let newTodoItem = new TodoItem(title, description, dueDate);
+  addItem(title, description, dueDate, priority) {
+    let newTodoItem = new TodoItem(title, description, dueDate, priority);
     this.items.push(newTodoItem);
   }
 }
--- src/modules/todoItem.js
+++ src/modules/todoItem.js
@@ -2,10 +2,11 @@ import { format, parseISO } from "date-fns";
 
 export default class TodoItem {
   static itemId = 1;
-  constructor(title, description, dueDate) {
+  constructor(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
     this.dueDate = format(parseISO(dueDate), "dd/MM/yyyy");
+    this.priority = priority;
     this.itemId = TodoItem.itemId++;
   }
 }
