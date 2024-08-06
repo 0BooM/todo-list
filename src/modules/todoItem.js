75c8378536854ade7dab811900f78cc533964597
0BooM <149960425+0BooM@users.noreply.github.com>
2024-08-06 16:44:34 +0200

    Update: todoItem now accepts date and changes it into correct format

--- src/modules/todoItem.js
+++ src/modules/todoItem.js
@@ -1,9 +1,11 @@
-class TodoItem {
+import { format, parseISO } from "date-fns";
+
+export default class TodoItem {
   static itemId = 1;
   constructor(title, description, dueDate) {
     this.title = title;
     this.description = description;
-    this.dueDate = dueDate;
+    this.dueDate = format(parseISO(dueDate), "dd/MM/yyyy");
     this.itemId = TodoItem.itemId++;
   }
 }
