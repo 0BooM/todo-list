647725ab4d6989cf17931e3491ee6bb140a925bf
0BooM <149960425+0BooM@users.noreply.github.com>
2024-08-07 17:04:58 +0200

    Update: Little changes/Add toJSON function

--- src/modules/project.js
+++ src/modules/project.js
@@ -2,20 +2,18 @@ import TodoItem from "./todoItem.js";
 
 export default class Project {
   static projectId = 1;
-  items = [
-    new TodoItem(
-      "Initial Task",
-      "This is an initial task description",
-      "2024-08-04",
-      "high",
-      "done"
-    ),
-  ];
+  items = [];
   constructor(title) {
     this.title = title;
     this.projectId = Project.projectId++;
   }
-  addItem(title, description, dueDate, priority, checked = false) {
+  addItem(
+    title,
+    description,
+    dueDate = "01/01/1999",
+    priority,
+    checked = false
+  ) {
     let newTodoItem = new TodoItem(
       title,
       description,
@@ -25,5 +23,12 @@ export default class Project {
     );
     this.items.push(newTodoItem);
     console.log(this.items);
+  }
+  toJSON() {
+    return {
+      title: this.title,
+      projectId: this.projectId,
+      items: this.items,
+    };
   }
 }
--- src/modules/todoItem.js
+++ src/modules/todoItem.js
@@ -1,13 +1,29 @@
-import { format, parseISO } from "date-fns";
+import { format, parseISO, isValid } from "date-fns";
 
 export default class TodoItem {
   static itemId = 1;
   constructor(title, description, dueDate, priority, checked = false) {
+    let parsedDate = parseISO(dueDate);
+    if (!isValid(parsedDate)) {
+      parsedDate = new Date(dueDate);
+    }
     this.title = title;
     this.description = description;
-    this.dueDate = format(parseISO(dueDate), "dd/MM/yyyy");
+    this.dueDate = isValid(parsedDate)
+      ? format(parsedDate, "dd/MM/yyyy")
+      : dueDate;
     this.priority = priority;
     this.status = checked ? "done" : "not-done";
     this.itemId = TodoItem.itemId++;
+  }
+  toJSON() {
+    return {
+      title: this.title,
+      description: this.description,
+      dueDate: this.dueDate,
+      priority: this.priority,
+      status: this.status,
+      itemId: this.itemId,
+    };
   }
 }
